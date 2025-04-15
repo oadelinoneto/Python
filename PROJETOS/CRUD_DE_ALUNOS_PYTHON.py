5f811c66ca7dc986067b10fbce471e5e91aa5cf9
oadelinoneto <martingalereddot@gmail.com>
2025-04-15 12:37:05 -0300

    Função de update_alunos() adicionada

--- PROJETOS/CRUD_DE_ALUNOS_PYTHON.py
+++ PROJETOS/CRUD_DE_ALUNOS_PYTHON.py
@@ -23,13 +23,26 @@ def render(banco):
         print('-------------')
         
 # CADASTRAR NOVO ALUNO
-def update_aluno(banco):
+def create_aluno(banco):
+
     nome = str(input('Digite o nome do aluno: '))
+    for aluno in banco:
+        if aluno['nome'] == nome:
+            print('Esse aluno já está cadastrado')
+            return 
+
+        
+                             
     idade = int(input('Digite a idade do aluno: '))
     curso = str(input('Digite o curso do aluno: '))
     novo_aluno = {'nome':nome,'idade':idade,'curso':curso}
+    
     atualizado = banco.append(novo_aluno)
+
+    print(f"Aluno {novo_aluno['nome']} cadastrado com sucesso")
+    
     return atualizado
+
 
 # LISTAR ALUNOS MAS APENAS O NOME
 def lista_alunos(banco):
@@ -39,9 +52,17 @@ def lista_alunos(banco):
 # PESQUISAR ALUNO POR NOME
 def pesquisar_alunos(banco):
     x = str(input('Digite o nome do aluno a pesquisar: '))
+    print('------------')
+    
     for aluno in banco:
+        
         if aluno['nome'] == x:
             print (f'Aluno {x} encontrando')
+            print('--------DADOS-------')
+            print(f"nome = {aluno['nome']}")
+            print(f"idade = {aluno['idade']}")
+            print(f"curso = {aluno['curso']}")
+            
             return None
         
     print (f'Aluno {x} não encontrado.')
@@ -54,32 +75,64 @@ def remover_alunos(banco):
     banco.pop(aluno_removido_id)
     return None
 
+
+#ATUALIZAR DADOS
+def update_aluno(banco):
+    lista_alunos(banco)
+    id = int(input('Digite o ID do aluno a ser alterado: '))
+    
+    banco[id]['nome'] = str(input('Digite o novo nome: '))
+    banco[id]['idade'] = str(input('Digite a nova idade: '))
+    banco[id]['curso'] = str(input('Digite o novo curso: '))
+    print(f'Aluno ID {id} atualizado!')    
+    
+    return None
+
 # MAIN
 def main():
     rodando = True
     alunos = []    
     while rodando:
-        
+        print('-------------------') 
         print('Seja bem vindo ao CRUD de teste de Adelino!')
         print('1 - Cadastrar aluno')
-        print('2 - Listar alunos')
+        print('2 - Listar dados alunos')
         print('3 - Remover aluno')
-        print('4 - Sair')
+        print('4 - Pesquisar aluno')
+        print('5 - Atualizar dados')
+        print('6 - Sair')
+        print('-----------------')
         
-        escolha = int(input())
+        escolha_valida = True
+        
+        while escolha_valida :
+            try:
+                escolha = int(input())
+                escolha_valida = False
+            except ValueError:
+                print('Valor inválido, escolha uma opção válida')
+        
+            
+                
+            
         if escolha == 1:
-            update_aluno(alunos)
-            render(alunos)
+            create_aluno(alunos)
+
         elif escolha == 2:
             render(alunos)
         elif escolha == 3:
             remover_alunos(alunos)
         elif escolha == 4:
-            rodando == False
-            break
+            pesquisar_alunos(alunos)
+        elif escolha ==5:
+            update_aluno(alunos)
+        elif escolha == 6:
+            rodando = False
             
     print('Tenha um bom dia!')
             
+   
+
                 
 main()
 
